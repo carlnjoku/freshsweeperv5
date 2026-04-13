09108125c25fa5501a5840d956055b407b46d9f6
Carl Njoku <carl>
2026-04-12 21:52:18 -0400

    April update

--- secret.js
+++
@@ -1,9 +0,0 @@
-// secrets.js
-// export const GOOGLE_MAPS_API_KEY = 'AIzaSyDJdAH7jfbpKNsiH9dhHpxx8Gxh2GN6Ew0';
-// export const GOOGLE_MAPS_API_KEY = 'AIzaSyDEcmSEw9ZtSb709NRO487hLTr1KiyGir8';
-// export const GOOGLE_MAPS_API_KEY = 'AIzaSyAMgD0ViZwm7zxSzKxTCV25EkjqfVONdUQ';
-// export const GOOGLE_MAPS_API_KEY = 'AIzaSyBBXa6LpNkw4hcxZ7tqWyOZPjfGF2Ir1CE';
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyBfj5tHpZNcRxbPwBEjPSJVCXtZ5mw9vOM';
-// export const PUBLISHABLE_KEY = 'pk_test_EFN6niSZSH7zAp3qWMHKcRWw'
-export const STRIPE_PUBLIC_SECRET_KEY ='pk_test_EFN6niSZSH7zAp3qWMHKcRWw'
-export const MAP_BOX_SECRET_KEY ='pk.eyJ1IjoiZnJlc2hzd2VlcGVyIiwiYSI6ImNtbWM1cmt4YzAydjEycW9hNXl0Mmx4ZzAifQ.qMQ47OCahW_fAw6WXyeOww'
